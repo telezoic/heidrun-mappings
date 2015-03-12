d16feca08466295206853be92bdc6e2a9a97a9eb
guegueng <gretchen.gueguen@gmail.com>
2015-03-12 11:23:18 -0400

    edits to indiana mapping
    all of the edits suggested by @anarchivist including adding the mapping
    for genre (which was also directed to @no-reply) because in retrospect
    I remember this conversation and I think this is right.

--- heidrun/indiana_dc.rb
+++ heidrun/indiana_dc.rb
@@ -3,7 +3,6 @@ Krikri::Mapper.define(:indiana_dc,
   provider :class => DPLA::MAP::Agent do
     uri 'http://dp.la/api/contributor/indiana'
     label 'Indiana Memory'
-    #I'm guessing here as to the naming of the above things
   end
 
   dataProvider :class => DPLA::MAP::Agent do
@@ -24,41 +23,46 @@ Krikri::Mapper.define(:indiana_dc,
 
   sourceResource :class => DPLA::MAP::SourceResource do
   
-  collection :class => DPLA::MAP::Collection, :each => record.field('xmlns:header', 'xmlns:setSpec'), :as => :coll do
-      title coll
-    end
+  	  collection :class => DPLA::MAP::Collection, :each => 
+  	  	  header.field('xmlns:header', 'xmlns:setSpec'), :as => :coll do
+  	  	  title coll
+  	  end
 
+  	  creator :class => DPLA::MAP::Agent, :each => 	
+  	  	  record.field('dc:creator'), :as => :creator do
+  	  	  providedLabel creator
+  	  end
 
-    creator :class => DPLA::MAP::Agent, :each => record.field('dc:creator'), :as => :creator do
-      providedLabel creator
-    end
+  	  date :class => DPLA::MAP::TimeSpan, :each => 
+  	  	  record.field('dc:date'), :as => :created do
+  	  	  providedLabel created
+  	  end
 
-    date :class => DPLA::MAP::TimeSpan, :each => record.field('dc:date'), :as => :created do
-      providedLabel created
-    end
+  	  description record.field('dc:description')
 
-    description record.field('dc:description')
+  	  dcformat record.fields('dc:format', 'dc:type')
+  	  
+  	  genre record.field('dc:type')
 
+  	  language :class => DPLA::MAP::Controlled::Language, :each => 
+  	  	  record.field('dc:language'), :as => :lang do
+  	  	  prefLabel lang
+  	  end
 
-    dcformat record.field('dc:format')
+  	  spatial :class => DPLA::MAP::Place, :each => 
+  	  	  record.field('dc:coverage'), :as => :place do
+  	  	  providedLabel place
+  	  end
 
-    language :class => DPLA::MAP::Controlled::Language, :each => record.field('dc:language'), :as => :lang do
-      prefLabel lang
-    end
+  	  rights record.field('dc:rights')
 
-    spatial :class => DPLA::MAP::Place, :each => record.field('dc:coverage'), :as => :place do
-      providedLabel place
-    end
+  	  subject :class => DPLA::MAP::Concept, :each => 	
+  	  	  record.field('dc:subject'), :as => :subject do
+  	  	  providedLabel subject
+  	  end
 
+  	  title record.field('dc:title')
 
-    rights record.field('dc:rights')
-
-    subject :class => DPLA::MAP::Concept, :each => record.field('dc:subject'), :as => :subject do
-      providedLabel subject
-    end
-
-    title record.field('dc:title')
-
-    dctype record.field('dc:type')
+  	  dctype record.field('dc:type')
   end
 end
